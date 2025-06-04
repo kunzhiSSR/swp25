10048a40cf1a080deea1104f9bad64b4e0457041
darmid23 <darmid233@gmail.com>
2025-06-04 03:44:56 +0200

    4

--- swp25fe/swp2025front/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.js   ✅ 推荐
-module.exports = {
-  plugins: [
-    require('tailwindcss'),   // <== 数组元素
-    require('autoprefixer'),
-  ],
-};
--- swp25fe/swp2025front/vitest.setup.js
+++
@@ -1,2 +0,0 @@
-// 在每个测试文件之前执行，相当于 Jest 的 setupFilesAfterEnv
-import '@testing-library/jest-dom';
